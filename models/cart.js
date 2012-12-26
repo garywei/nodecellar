f184210b91535712c31d4cac9911b26c6db605d6
Gary Wei <gary@fooda.com>
2012-12-26 09:28:04 -0600

    create new model

---
+++ models/cart.js
@@ -0,0 +1,21 @@
+// cart schema
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+
+var CartSchema = new Schema({
+    hgId: {type : String}
+  , UserId: {type : String}
+  , Status: {type : String, default : 'Active', trim : true}
+  , LineItems: [] // Item sku, quantity, 
+  , CreatedDate  : {type : Date, default : Date.now}
+  , ModifiedDate  : {type : Date, default : Date.now}
+});
+
+CartSchema.path('UserId').validate(function (UserId) {
+  return UserId.length > 0
+}, 'User Id cannot be blank')
+
+
+mongoose.model('CartModel', ArticleSchema)
